801199afd7bc69099029061e31c34e19dc35bfd9
Maksymilian Galas <maksg@onet.eu>
2019-02-18 19:46:43 +0100

    Cleanup code and fix brightness slider

---
+++ edytor graficzny/edytor-graficzny.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', appStart);
+
+let canvas;
+let ctx;
+let imageData;
+
+function appStart() {
+    canvas = document.querySelector('#ps');
+    ctx = canvas.getContext('2d');
+
+    let img = new Image();
+    //img.src = 'https://picsum.photos/600/300/?random=1';
+    img.src = './grafika.jpg';
+    img.addEventListener('load', () => {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    })
+
+    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(imageData);
+
+    let brightnessSlider = document.querySelector('#brightness');
+
+    brightnessSlider.oninput = function () {
+        brightnessChange(this.value);
+    }
+}
+
+function brightnessChange(value) {
+    canvas.style.filter = `brightness(${value}%)`;
+}
