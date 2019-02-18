23376fb43928dd984b57caecc7c84c713298caf3
Maksymilian Galas <maksg@onet.eu>
2019-02-18 20:46:41 +0100

    Add more filters

--- edytor graficzny/edytor-graficzny.js
+++ edytor graficzny/edytor-graficzny.js
@@ -5,6 +5,9 @@ let canvas;
 var brightness = 100;
 var contrast = 100;
 var saturation = 100;
+var blur = 0;
+var sepia = 0;
+var invert = 0;
 
 function appStart() {
     canvas = document.querySelector('#ps');
@@ -18,21 +21,33 @@ function appStart() {
     })
 
     let brightnessSlider = document.querySelector('#brightness');
-
     brightnessSlider.oninput = function () {
         brightnessChange(this.value);
     }
 
     let contrastSlider = document.querySelector('#contrast');
-
     contrastSlider.oninput = function () {
         contrastChange(this.value);
     }
 
     let saturationSlider = document.querySelector('#saturation');
-
     saturationSlider.oninput = function () {
         saturationChange(this.value);
+    }
+
+    let blurSlider = document.querySelector('#blur');
+    blurSlider.oninput = function () {
+        blurChange(this.value);
+    }
+
+    let sepiaSlider = document.querySelector('#sepia');
+    sepiaSlider.oninput = function () {
+        sepiaChange(this.value);
+    }
+
+    let invertSlider = document.querySelector('#invert');
+    invertSlider.oninput = function () {
+        invertChange(this.value);
     }
 }
 
@@ -51,6 +66,21 @@ function saturationChange(value) {
     setFilters();
 }
 
+function blurChange(value) {
+    blur = value;
+    setFilters();
+}
+
+function sepiaChange(value) {
+    sepia = value;
+    setFilters();
+}
+
+function invertChange(value) {
+    invert = value;
+    setFilters();
+}
+
 function setFilters() {
-    canvas.style.filter = `brightness(${brightness}%) contrast(${contrast}%) saturate(${saturation}%)`;
+    canvas.style.filter = `brightness(${brightness}%) contrast(${contrast}%) saturate(${saturation}%) blur(${blur}px) sepia(${sepia}%) invert(${invert}%)`;
 }
