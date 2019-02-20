3aa8775220e25a5a5c027d88a1542ebcc9df17b7
Maksymilian Galas <maksg@onet.eu>
2019-02-20 20:55:29 +0100

    Fix player movement and add holes

--- ball in hole/ball-in-a-hole.js
+++ ball in hole/ball-in-a-hole.js
@@ -1,28 +1,120 @@
 document.addEventListener('DOMContentLoaded', appStart);
 
+var canvas;
+var ctx;
+var player;
+var holes = [];
+
 function appStart() {
-    let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    var size = { width: canvas.width, height: canvas.height };
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
-    var player = new Player(ctx);
-    player.setPosition(size.width / 2, size.height / 2);
-
-    window.addEventListener('deviceorientation', function(event) {
-        moveMe(event, player, size);
-    });
+    startGame();
+    
+    window.addEventListener('deviceorientation', moveMe);
 
     animate();
 
     function animate() {
-        drawFrame(player, ctx, size);
+        player.update();
+
+        holes.forEach(function (hole) {
+            player.collidesWithHole(hole);
+        });
+
+        drawFrame();
         requestAnimationFrame(animate);
     }
 }
 
-function Player(ctx) {
-    var position = { x: 0, y: 0 };
-    var size = { width: 30, height: 30 };
+function startGame() {
+    player = new Player();
+    player.setPosition(canvas.width / 2, canvas.height / 2);
+
+    for (var i = 0; i < 20; i++) {
+        var hole = new Hole()
+        var x = Math.floor((Math.random() * canvas.width - hole.getSize().width * 2) + hole.getSize().width * 2);
+        var y = Math.floor((Math.random() * canvas.height - hole.getSize().height * 2) + hole.getSize().height * 2);
+        hole.setPosition(x, y);
+        holes.push(hole);
+    }
+
+    holes[0].setActive();
+}
+
+function Player() {
+    var position = { x: 0.0, y: 0.0 };
+    var size = { width: 20.0, height: 20.0 };
+    var speed = { x: 0.0, y: 0.0 };
+
+    this.setPosition = function (x, y) {
+        if (x < size.width) {
+            x = size.width;
+        } else if (x > canvas.width - size.width) {
+            x = canvas.width - size.width;
+        }
+        position.x = x;
+
+        if (y < size.height) {
+            y = size.height;
+        } else if (y > canvas.height - size.height) {
+            y = canvas.height - size.height;
+        }
+        position.y = y;
+    };
+
+    this.getPosition = function () {
+        return position;
+    };
+
+    this.setSpeed = function (x, y) {
+        speed.x = x;
+        speed.y = y;
+    };
+
+    this.getSpeed = function () {
+        return speed;
+    };
+
+    this.getSize = function () {
+        return size;
+    };
+
+    this.render = function () {
+        ctx.beginPath();
+        ctx.arc(position.x, position.y, size.width, 0, 2 * Math.PI);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    }
+
+    this.update = function () {
+        this.setPosition(position.x + speed.x, position.y + speed.y);
+    }
+
+    this.collidesWithHole = function (hole) {
+        var dx = position.x - hole.getPosition().x;
+        var dy = position.y - hole.getPosition().y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < size.width + hole.getSize().width) {
+            if (hole.isActive()) {
+                holes.splice(0, 1);
+                if (holes.length > 0) {
+                    holes[0].setActive();
+                }
+            } else {
+                var x = Math.floor((Math.random() * canvas.width - size.width * 2) + size.width * 2);
+                var y = Math.floor((Math.random() * canvas.height - size.height * 2) + size.height * 2);
+                this.setPosition(x, y);
+            }
+        }
+    }
+}
+
+function Hole() {
+    var position = { x: 0.0, y: 0.0 };
+    var size = { width: 20.0, height: 20.0 };
+    var active = false;
 
     this.setPosition = function (x, y) {
         position.x = x;
@@ -37,28 +129,41 @@ function Player(ctx) {
         return size;
     };
 
+    this.setActive = function () {
+        active = true;
+    }
+
+    this.isActive = function () {
+        return active;
+    }
+
     this.render = function () {
         ctx.beginPath();
         ctx.arc(position.x, position.y, size.width, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
+        if (active) {
+            ctx.fillStyle = "green";
+        } else {
+            ctx.fillStyle = "black";
+        }
         ctx.fill();
     }
 }
 
-function drawFrame(player, ctx, size) {
-    ctx.clearRect(0, 0, size.width, size.height);
+function drawFrame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    ctx.rect(0, 0, size.width, size.height);
+    ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgb(197,126,20)";
     ctx.fill();
+
+    holes.forEach(function (hole) {
+        hole.render();
+    });
 
     player.render();
 }
 
-function moveMe(event, player, size) {
-    var widthMultiplier = size.width / 180;
-    var heightMultiplier = size.height / 180;
-    player.setPosition(size.width / 2 + event.gamma * widthMultiplier, size.height / 2 + event.beta * heightMultiplier);
-    console.log(event);
+function moveMe(event) {
+    player.setSpeed(event.gamma / 2, event.beta / 2);
 }
