9fc618c04ffa95bbd5b792d199ff1225d81fc749
Maksymilian Galas <maksg@onet.eu>
2019-02-22 21:34:13 +0100

    Add note object

--- notepocket/notepocket.js
+++ notepocket/notepocket.js
@@ -2,15 +2,40 @@ document.addEventListener('DOMContentLoaded', appStart);
 
 var selectedNote = 0;
 
+const NoteColor = {
+    green: "green",
+    red: "red"
+};
+
+function Note(title, content, color, pinned, createdDate) {
+    this.title = title;
+    this.content = content;
+    this.color = color;
+    this.pinned = pinned;
+    this.createdDate = createdDate;
+}
+
 function appStart() {
+    var notes = [new Note("Test 1", "sdgs gsggdgsdg sdsd ", NoteColor.red, false, Date.now()),
+    new Note("Test Drugi", "hhhhhhh ", NoteColor.green, true, Date.now())];
+
     var newNoteButton = document.querySelector('#newNoteButton');
-    newNoteButton.addEventListener('click', createNewNote);
+    newNoteButton.addEventListener('click', function (e) {
+        var note = new Note("", "", NoteColor.green, false, Date.now());
+        addNote(note);
+        notes.push(note);
+    });
+
+    notes.forEach( note => {
+        console.log(note);
+        addNote(note);
+    });
     
     var noteTitle = document.querySelector('#noteTitle');
     var noteText = document.querySelector('#noteText');
 }
 
-function createNewNote(e) {
+function addNote(note) {
     var notesTable = document.querySelector('#notesTable');
     var notesTableCells = notesTable.querySelectorAll('tr td');
     notesTableCells.forEach(cell => {
@@ -19,7 +44,7 @@ function createNewNote(e) {
     var row = notesTable.insertRow(0);
     var cell = row.insertCell(0);
     cell.style.backgroundColor = 'red';
-    cell.innerHTML = '<b>Tytuł notatki</b><br><p>Treść notatki</p>';
+    cell.innerHTML = `<b>${note.title}</b><br><p>${note.content}</p>`;
     //cell.addEventListener('click', selectNote);
     
     notesTableCells = notesTable.querySelectorAll('tr td');
